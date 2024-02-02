188e6f3eab42efb21a84042720713e73581e749f
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-03 00:22:31 +0600

    3

---
+++ 3_Hypotenuse of a right triangle.py
@@ -0,0 +1,11 @@
+#Litu Ahamed Nirob(221-33-1534)
+#3. Calculate the hypotenuse of a right triangle
+import math
+a = 12
+b = 9
+c_squared = a**2 + b**2
+c = math.sqrt(c_squared)
+
+print("The hypotenuse of the right triangle is:", c)
+
+
