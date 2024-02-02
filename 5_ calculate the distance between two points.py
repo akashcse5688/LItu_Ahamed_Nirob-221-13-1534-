2b867766c2c6a327f784afae685cd60cfd9bd8c0
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-03 00:23:14 +0600

    5

---
+++ 5_ calculate the distance between two points.py
@@ -0,0 +1,10 @@
+#LituAhamedNirob(221-33-1534)
+#calculate the distance between two points on a 2D plane
+import math
+x1=12
+x2=14
+y1=15
+y2=10
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print("The distance between the two points is: ",distance)
