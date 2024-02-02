886d43c6ecda4c74565b915d0d83d3a0e28f6e84
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-03 00:22:52 +0600

    4

---
+++ 4_calculate the circumference of a circle.py
@@ -0,0 +1,9 @@
+
+#3. Calculate the hypotenuse of a right triangle
+import math
+a = 12
+b = 9
+c_squared = a**2 + b**2
+c = math.sqrt(c_squared)
+
+print("The hypotenuse of the right triangle is:", c)
