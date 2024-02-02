6da55153d10c6cd1c5a025bae01b8caf1447543c
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-03 00:23:50 +0600

    Experiment

---
+++ Experiment Code.py
@@ -0,0 +1,6 @@
+##Litu AhamedNirob(221-33-1534)
+# Calculate the area using the formula: area = length * width
+length = 8
+width = 5
+area = length * width
+print("The area of the rectangle is:", area)
