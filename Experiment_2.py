7ddbea1aa4f0e1447ebe2f749e291c78001feb80
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-10 00:18:36 +0600

    Experiment_2

---
+++ Experiment_2.py
@@ -0,0 +1,12 @@
+#Litu Ahamed Nirob(221-33-1534)
+#calculate the volume
+length=5.5
+breadth=0.102
+weight=3.5
+
+volume=(length*breadth*weight)
+print("The volume is= ",volume)
+#Type of The data Tuype
+print(type(length))
+print(type(breadth))
+print(type(weight))
