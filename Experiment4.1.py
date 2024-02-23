576be06979c57b9d2822648e35c5e5ba07c00d32
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-23 23:49:35 +0600

    Experiment4.1

---
+++ Experiment4.1.py
@@ -0,0 +1,8 @@
+# Litu Ahamed Nirob(221-33-1534)
+n = int(input("Enter a number= "))
+if (n < 0):
+    print("Negative number ")
+elif (n > 0):
+    print("Positive number")
+else:
+    print("Zero")
