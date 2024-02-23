4e8c405189737d05273c7357d3b0bbe9cb0cc703
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-23 23:50:06 +0600

    Experiment4.2

---
+++ Experiment4.2.py
@@ -0,0 +1,10 @@
+# Litu Ahamed Nirob(221-33-1534)
+a = int(input("Enter First number: "))
+b = int(input("Enter Second number: "))
+c = int(input("Enter Third number: "))
+if (a > b and a > c):
+    print("First number is large number ")
+elif (b > a and b > c):
+    print("Second number is large number ")
+else:
+    print("Third number is large number ")
