2e7430929452ec6124d5e4b4ecf2552f4667ebdb
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-23 20:14:07 +0600

    Expeiment3.2

---
+++ Experiment3.2.py
@@ -0,0 +1,7 @@
+# Litu Ahamed Nirob(221-33-1534)
+pi = 3.1416
+radius = float(input("Please Enter the radius of a circle: "))
+circumference = 2 * pi * radius
+area = pi * radius * radius
+print("Area Of a Circle = ", area)
+print("Circumference Of a Circle = ", circumference)
