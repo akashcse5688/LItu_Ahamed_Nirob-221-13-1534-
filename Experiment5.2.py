844c8ad5d074a1ad6b9a5d6b8dab2d11d4e8193d
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-24 17:39:12 +0600

    5.2 and 5.2

---
+++ Experiment5.2.py
@@ -0,0 +1,8 @@
+# Litu Ahamed Nirob(221-33-1534)
+sum = 0
+i = 1
+while i <= 100:
+    if i % 2 != 0:
+        sum = sum + i
+    i = i + 1
+print("The sum of odd numbers from 1 to 100 is =", sum)
---
+++ Experiment5.3.py
@@ -0,0 +1,4 @@
+# Litu Ahamed Nirob(221-33-1534)
+rows = 5
+for i in range(rows, 0, -1):
+    print("*" * i)
